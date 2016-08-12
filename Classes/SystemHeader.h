99067db4d4b497650d71429f573b8d63b9df7fac
qzl1994 <305165598@qq.com>
2016-08-12 18:39:48 +0800

    添加声音资源，定义一个通用头文件SystemHeader.h,其中定义了一些通用宏和全局常亮

---
+++ Classes/SystemHeader.h
@@ -0,0 +1,24 @@
+#include "SimpleAudioEngine.h"
+
+#define SOUND_KEY "sound_key"
+#define MUSIC_KEY "music_key"
+
+#define HIGHSCORE_KEY "highscore_key"
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+
+#define bg_music_1 "sound/home_bg.aifc"
+#define bg_music_2 "sound/game_bg.aifc"
+#define sound_1 "sound/Blip.caf"
+#define sound_2 "sound/Explosion.caf"
+
+#else
+
+#define bg_music_1 "sound/home_bg.mp3"
+#define bg_music_2 "sound/game_bg.mp3"
+#define sound_1 "sound/Blip.wav"
+#define sound_2 "sound/Explosion.wav"
+
+#endif
+
+using namespace CocosDenshion;
