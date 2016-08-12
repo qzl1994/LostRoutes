bd08543a3e12b7959c3a301bc6c9cea2d97593dc
qzl1994 <305165598@qq.com>
2016-08-12 16:56:44 +0800

    添加两个资源目录，修改分辨率策略，支持多分辨率

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -3,10 +3,9 @@
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
-static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
+static cocos2d::Size designResolutionSize = cocos2d::Size(320, 568);
+static cocos2d::Size smallResolutionSize = cocos2d::Size(640, 1136);
+static cocos2d::Size largeResolutionSize = cocos2d::Size(750, 1334);
 
 AppDelegate::AppDelegate()
 {
@@ -16,25 +15,20 @@ AppDelegate::~AppDelegate()
 {
 }
 
-// if you want a different context, modify the value of glContextAttrs
-// it will affect all platforms
 void AppDelegate::initGLContextAttrs()
 {
-    // set OpenGL context attributes: red,green,blue,alpha,depth,stencil
     GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
 
     GLView::setGLContextAttrs(glContextAttrs);
 }
 
-// if you want to use the package manager to install more packages,  
-// don't modify or remove this function
 static int register_all_packages()
 {
     return 0; //flag for packages manager
 }
 
 bool AppDelegate::applicationDidFinishLaunching() {
-    // initialize director
+	// 初始化导演
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
     if(!glview) {
@@ -46,43 +40,43 @@ bool AppDelegate::applicationDidFinishLaunching() {
         director->setOpenGLView(glview);
     }
 
-    // turn on display FPS
-    director->setDisplayStats(true);
+    director->setDisplayStats(false);
 
-    // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0f / 60);
 
-    // Set the design resolution
-    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
+    // 设计分辨率策略
+    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::FIXED_WIDTH);
+
+	// 屏幕分辨率
     auto frameSize = glview->getFrameSize();
-    // if the frame's height is larger than the height of medium size.
-    if (frameSize.height > mediumResolutionSize.height)
-    {        
-        director->setContentScaleFactor(MIN(largeResolutionSize.height/designResolutionSize.height, largeResolutionSize.width/designResolutionSize.width));
-    }
-    // if the frame's height is larger than the height of small size.
-    else if (frameSize.height > smallResolutionSize.height)
-    {        
-        director->setContentScaleFactor(MIN(mediumResolutionSize.height/designResolutionSize.height, mediumResolutionSize.width/designResolutionSize.width));
-    }
-    // if the frame's height is smaller than the height of medium size.
-    else
-    {        
-        director->setContentScaleFactor(MIN(smallResolutionSize.height/designResolutionSize.height, smallResolutionSize.width/designResolutionSize.width));
-    }
+
+	std::vector<std::string> searchPaths;
+
+	// 如果屏幕分辨率高度大于small尺寸的资源分辨率高度，选择large资源
+	if (frameSize.height > smallResolutionSize.height)
+	{
+		director->setContentScaleFactor(MIN(largeResolutionSize.height / designResolutionSize.height, largeResolutionSize.width / designResolutionSize.width));
+		searchPaths.push_back("large");
+	}
+	// 如果屏幕分辨率高度小于或等于small尺寸的资源分辨率高度，选择small资源
+	else
+	{
+		director->setContentScaleFactor(MIN(smallResolutionSize.height / designResolutionSize.height, smallResolutionSize.width / designResolutionSize.width));
+		searchPaths.push_back("small");
+	}
+
+	// 设置资源搜索路径
+	FileUtils::getInstance()->setSearchPaths(searchPaths);
 
     register_all_packages();
 
-    // create a scene. it's an autorelease object
     auto scene = HelloWorld::createScene();
 
-    // run
     director->runWithScene(scene);
 
     return true;
 }
 
-// This function will be called when the app is inactive. Note, when receiving a phone call it is invoked.
 void AppDelegate::applicationDidEnterBackground() {
     Director::getInstance()->stopAnimation();
 
--- Classes/AppDelegate.h
+++ Classes/AppDelegate.h
@@ -3,11 +3,6 @@
 
 #include "cocos2d.h"
 
-/**
-@brief    The cocos2d Application.
-
-Private inheritance here hides part of interface from Director.
-*/
 class  AppDelegate : private cocos2d::Application
 {
 public:
@@ -16,23 +11,10 @@ public:
 
     virtual void initGLContextAttrs();
 
-    /**
-    @brief    Implement Director and Scene init code here.
-    @return true    Initialize success, app continue.
-    @return false   Initialize failed, app terminate.
-    */
     virtual bool applicationDidFinishLaunching();
 
-    /**
-    @brief  Called when the application moves to the background
-    @param  the pointer of the application
-    */
     virtual void applicationDidEnterBackground();
 
-    /**
-    @brief  Called when the application reenters the foreground
-    @param  the pointer of the application
-    */
     virtual void applicationWillEnterForeground();
 };
 
